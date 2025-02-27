1d2d12ad9c58149e7b4aa33afbf1b682b798563b
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-02-27 15:40:36 -0300

    Initial commit

---
+++ src/main/java/br/com/caotica/CaoticaApplication.java
@@ -0,0 +1,22 @@
+package br.com.caotica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application boot class
+ * @author Thiago Pinheiro do Nascimento
+ * @since 06 feb 2025
+**/
+@SpringBootApplication
+public class CaoticaApplication {
+
+	/**
+	 * Main application method
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 06 feb 2025
+	**/
+	public static void main(String[] args) {
+		SpringApplication.run(CaoticaApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/caotica/CaoticaApplicationTests.java
@@ -0,0 +1,23 @@
+package br.com.caotica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Prototype class intended for test implementation
+ * @author Thiago Pinheiro do Nascimento
+ * @since 06 feb 2025
+**/
+@SpringBootTest
+class CaoticaApplicationTests {
+
+	/**
+	 * Method intended for implementing tests
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 06 feb 2025
+	**/
+	@Test
+	void contextLoads() {
+	
+	}
+}
