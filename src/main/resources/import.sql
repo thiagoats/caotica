db8f874b8becd6f42fd51cc31dbb9d147557b000
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-03-01 11:10:58 -0300

    Import of an fictitious associate

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,3 @@
 INSERT INTO users (username, password, authority) VALUES ("admin@caotica.br", "$2a$10$WD8yeBjJ.1bJpmMcKP5MPe19Os/3ZQio3uZ8xOj0BJTxrPNYcWtaO", "ADMIN");
+
+INSERT INTO associates (cpf, name, birth, gender) VALUES ("24757689080", "Joaquim José da Silva Xavier", "1792-11-12", "MAL");
