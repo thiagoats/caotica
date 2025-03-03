791ca76aec384ef1a7aab0a4a70133a2e96c786d
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-03-03 10:54:23 -0300

    Javascript files

---
+++ src/main/resources/static/js/general/grid.js
@@ -0,0 +1,6 @@
+$(".grid").DataTable({
+  "responsive": true, "lengthChange": false, "autoWidth": false,
+  "language": {
+      "url": "https://cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json"
+  }
+});
