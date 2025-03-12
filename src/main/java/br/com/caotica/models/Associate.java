6b482449c2a02ec3bbf32fdf50946332575c8004
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-03-12 10:39:20 -0300

    Associate validations

--- src/main/java/br/com/caotica/models/Associate.java
+++ src/main/java/br/com/caotica/models/Associate.java
@@ -2,6 +2,11 @@ package br.com.caotica.models;
 
 import java.util.Date;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,6 +21,8 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -41,18 +48,24 @@ public class Associate {
 	/**
 	 * Associate cpf
 	**/
+	@CPF(message = "CPF é inválido")
+	@NotBlank(message = "CPF é obrigatório")
 	@Column(columnDefinition = "char(11)", nullable = false, updatable = false)
 	private String cpf;
 	
 	/**
 	 * Associate name
 	**/
+	@Length(min = 3, max = 50, message = "Nome deve conter entre 3 e 50 caracteres")
+	@NotBlank(message = "Nome é obrigatório")
 	@Column(length = 50, nullable = false)
 	private String name;
 	
 	/**
 	 * Associate birth
 	**/
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@NotNull(message = "Nascimento é obrigatório")
 	@Column(columnDefinition = "date", nullable = false)
 	private Date birth;
 	
@@ -60,6 +73,7 @@ public class Associate {
 	 * Associate gender
 	**/
 	@Enumerated(EnumType.STRING)
+	@NotNull(message = "Sexo é obrigatório")
 	@Column(columnDefinition = "char(3)", nullable = false)
 	private Gender gender;
 	
