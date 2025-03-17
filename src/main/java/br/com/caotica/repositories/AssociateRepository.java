8df2a12210e1e57b439589b3eb27b064cb13a37f
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-03-17 19:20:44 -0300

    Optionally find associate by CPF

--- src/main/java/br/com/caotica/repositories/AssociateRepository.java
+++ src/main/java/br/com/caotica/repositories/AssociateRepository.java
@@ -1,5 +1,7 @@
 package br.com.caotica.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,10 @@ import br.com.caotica.models.Associate;
 @Repository
 public interface AssociateRepository extends JpaRepository<Associate, Long> {
 
+	/**
+	 * Optionally find associate by cpf
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 13 mar 2025
+	**/
+	public Optional<Associate> findByCpf(String cpf);
 }
