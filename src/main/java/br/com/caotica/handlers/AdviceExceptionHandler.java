2f762fcf4e266f5ffc25697d4f0c5979debba9f6
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-03-19 12:04:03 -0300

    Controller advice exception handlers

---
+++ src/main/java/br/com/caotica/handlers/AdviceExceptionHandler.java
@@ -0,0 +1,67 @@
+package br.com.caotica.handlers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import br.com.caotica.exceptions.EntityNotFoundException;
+
+/**
+ * Controller advice exception handlers
+ * @author Thiago Pinheiro do Nascimento
+ * @since 13 mar 2025
+**/
+@ControllerAdvice
+public class AdviceExceptionHandler {
+
+	/**
+	 * Redirection for catching illegal argument exception
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 13 mar 2025
+	**/
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String handleIllegalArgumentException(IllegalArgumentException e) {
+		return "errors/400";
+	}
+	
+	/**
+	 * Redirection for catching entity not found exception
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 13 mar 2025
+	**/
+	@ExceptionHandler(EntityNotFoundException.class)
+	public String handleEntityNotFound(EntityNotFoundException e) {
+		return "errors/404";
+	}
+	
+	/**
+	 * Redirection for catching no handle exception
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 13 mar 2025
+	**/
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public String handleNoHandleFound(NoHandlerFoundException e) {
+		return "errors/404";
+	}
+	
+	/**
+	 * Redirection for catching no resource exception
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 13 mar 2025
+	**/
+	@ExceptionHandler(NoResourceFoundException.class)
+	public String handleNoResourceFound(NoResourceFoundException e) {
+		return "errors/404";
+	}
+	
+	/**
+	 * Redirection for catching exception
+	 * @author Thiago Pinheiro do Nascimento
+	 * @since 13 mar 2025
+	**/
+	@ExceptionHandler(Exception.class)
+	public String handleUncaught(Exception e) {
+		return "errors/500";
+	}
+}
