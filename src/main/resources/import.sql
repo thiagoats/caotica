2508588ab42f3592836519c732f0c156521ce8ab
Thiago Pinheiro <thiagopn.dev@gmail.com>
2025-03-25 18:25:50 -0300

    Update default user import with audit data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (username, password, authority) VALUES ("admin@caotica.br", "$2a$10$WD8yeBjJ.1bJpmMcKP5MPe19Os/3ZQio3uZ8xOj0BJTxrPNYcWtaO", "ADMIN");
 
-INSERT INTO associates (cpf, name, birth, gender) VALUES ("24757689080", "Joaquim José da Silva Xavier", "1792-11-12", "MAL");
+INSERT INTO associates (cpf, name, birth, gender, created_at, created_by, updated_at, updated_by) VALUES ("24757689080", "Joaquim José da Silva Xavier", "1792-11-12", "MAL", utc_timestamp, "admin@caotica.br", utc_timestamp, "admin@caotica.br");
 
 INSERT INTO addresses (associates_id, place, complement, district) VALUES (1, "Rua da Inconfidência Mineira", "Casa 999-A", "Contagem");
 
